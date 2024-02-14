57897f24030f5c48c5b9401f3dbdf96c4eee660c
Elena Shulevska <shulevskaelena@gmail.com>
2024-02-14 22:18:54 +0100

    about us page refactored

--- food_power/lib/views/AboutUs.dart
+++ food_power/lib/views/AboutUs.dart
@@ -1,65 +1,88 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:food_power/widgets/CustomCard.dart';
 
-class AboutUsPage extends StatelessWidget {
-  Color myColor = Color(0xffF1CE74);
+class AboutUsPage extends StatefulWidget {
+  @override
+  _AboutUsPageState createState() => _AboutUsPageState();
+}
+
+class _AboutUsPageState extends State<AboutUsPage> with TickerProviderStateMixin {
+  late AnimationController _slideControllerLeft;
+  late Animation<Offset> _slideAnimationLeft;
+  late AnimationController _slideControllerRight;
+  late Animation<Offset> _slideAnimationRight;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _slideControllerLeft = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _slideAnimationLeft = Tween<Offset>(begin: Offset(-1.0, 0.0), end: Offset.zero).animate(
+      CurvedAnimation(parent: _slideControllerLeft, curve: Curves.easeInOut),
+    );
+
+    _slideControllerRight = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+    _slideAnimationRight = Tween<Offset>(begin: Offset(1.0, 0.0), end: Offset.zero).animate(
+      CurvedAnimation(parent: _slideControllerRight, curve: Curves.easeInOut),
+    );
+
+    _slideControllerLeft.forward();
+    _slideControllerRight.forward();
+  }
+
+  @override
+  void dispose() {
+    _slideControllerLeft.dispose();
+    _slideControllerRight.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:  Color(0xffF1CE74),
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: Text('About Us', style: GoogleFonts.nunitoSans(
-          textStyle: TextStyle(color: Colors.amber, letterSpacing: .5, fontSize: 25, fontWeight: FontWeight.bold,),)),
+          textStyle: TextStyle(color: Colors.teal[700], letterSpacing: .5, fontSize: 25, fontWeight: FontWeight.bold),
+        )),
         backgroundColor: Colors.transparent,
         elevation: 0,
         foregroundColor: Colors.black,
+        iconTheme: IconThemeData(color: Colors.black45),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Empowering Your Workplace Meals',
-                  style: GoogleFonts.nunitoSans(
-                    textStyle: TextStyle(color: Colors.black, letterSpacing: .5, fontSize: 25, fontWeight: FontWeight.bold,),)
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SlideTransition(
+                position: _slideAnimationLeft,
+                child: CustomCard(
+                  title: 'Empowering Your Workplace Meals',
+                  text: 'Food Power revolutionizes the way you enjoy meals at your workplace. Our web platform simplifies the process of selecting and booking hot meals for the next day, ensuring you always have access to delicious, ready-to-eat options. Whether you are at work or planning ahead from home, Food Power brings the convenience of choosing from a curated menu right to your fingertips.',
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Food Power revolutionizes the way you enjoy meals at your workplace. Our web platform simplifies the process of selecting and booking hot meals for the next day, ensuring you always have access to delicious, ready-to-eat options. Whether you are at work or planning ahead from home, Food Power brings the convenience of choosing from a curated menu right to your fingertips.',
-                  style: GoogleFonts.nunitoSans(
-                    textStyle: TextStyle(color: Colors.black, letterSpacing: .5, fontSize: 20, ),)
+              SizedBox(height: 30),
+              SlideTransition(
+                position: _slideAnimationRight,
+                child: CustomCard(
+                  title: 'Convenience at Its Core',
+                  text: 'Our platform stands out by offering comprehensive meal overviews, enabling users to select not only their preferred dishes but also choose convenient pick-up or delivery times. The dual-access system ensures that both customers and employees can navigate the platform with ease, maintaining a clear view of order statuses.',
+                ),
               ),
-            ),
-            // You can continue adding more sections here
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Convenience at Its Core',
-                  style: GoogleFonts.nunitoSans(
-                    textStyle: TextStyle(color: Colors.black, letterSpacing: .5, fontSize: 25, fontWeight: FontWeight.bold,),)
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                'Our platform stands out by offering comprehensive meal overviews, enabling users to select not only their preferred dishes but also choose convenient pick-up or delivery times. The dual-access system ensures that both customers and employees can navigate the platform with ease, maintaining a clear view of order statuses.',
-                  style: GoogleFonts.nunitoSans(
-                    textStyle: TextStyle(color: Colors.black, letterSpacing: .5, fontSize: 20,),)
-              ),
-            ),
-            // Include images, videos, or other media to elaborate on your "About Us" narrative
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset('lib/assets/food.gif'), // Replace with your asset name
-              ),
-            ),
-            // ... additional content ...
-          ],
+              SizedBox(height: 30),
+              Center(child: Image.asset('lib/assets/food-cooker.gif')),
+              // Additional content here
+            ],
+          ),
         ),
       ),
     );
---
+++ food_power/lib/widgets/CustomCard.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomCard extends StatelessWidget {
+  final String title;
+  final String text;
+  final Color bgColor;
+
+  const CustomCard({
+    Key? key,
+    required this.title,
+    required this.text,
+    this.bgColor = const Color(0xFFF0EDEE), // A color between white and dirty white
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15.0),
+      ),
+      color: bgColor,
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              title,
+              style: GoogleFonts.nunitoSans(
+                textStyle: TextStyle(
+                  color: Colors.black,
+                  letterSpacing: .5,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              text,
+              style: GoogleFonts.nunitoSans(
+                textStyle: TextStyle(
+                  color: Colors.black,
+                  letterSpacing: .5,
+                  fontSize: 18,
+                  height: 1.5,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
